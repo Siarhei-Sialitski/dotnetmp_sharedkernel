d1d288917352bcd6cd8c0bab2b27f8f8d9a08d29
Siarhei Sialitski <siarhei.sialitski.ws@gmail.com>
2022-06-05 22:01:32 +0300

    Not Found Exception Added

---
+++ DotNetMP.SharedKernel/Exceptions/NotFoundException.cs
@@ -0,0 +1,13 @@
+﻿namespace DotNetMP.SharedKernel.Exceptions;
+
+public class NotFoundException : Exception
+{
+    public NotFoundException() : this("Not found.")
+    { }
+
+    public NotFoundException(string message) : base(message)
+    { }
+
+    public NotFoundException(string message, Exception innerException) : base(message, innerException)
+    { }
+}
--- DotNetMP.SharedKernel/Interfaces/IRepository.cs
+++ DotNetMP.SharedKernel/Interfaces/IRepository.cs
@@ -6,6 +6,6 @@ public interface IRepository<T> where T : class, IAggregateRoot
     Task UpdateAsync(T entity, CancellationToken cancellationToken = default);
     Task DeleteAsync(T entity, CancellationToken cancellationToken = default);
     Task<T?> GetByIdAsync<TId>(TId id, CancellationToken cancellationToken = default) where TId : notnull;
-    Task<List<T>> ListAsync(CancellationToken cancellationToken = default);
+    Task<IList<T>> ListAsync(CancellationToken cancellationToken = default);
     Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
 }
